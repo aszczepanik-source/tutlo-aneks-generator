ba8657ce2894678a883449c9097da13ebf8b7943
aszczepanik-source <a.szczepanik@tutlo.pl>
2026-07-23 13:44:21 +0200

    Configure Apps Script templates

--- config.js
+++ config.js
@@ -1,1 +1,2 @@
-export const APPS_SCRIPT_URL = "...";
+export const APPS_SCRIPT_URL =
+  'https://script.google.com/a/macros/tutlo.pl/s/AKfycbzBMRxm3yFIjz7XI72FvHEf7uRwy3eDHwbYvCKN04t9fEQNHeJ1cvPPJzKEv_Ct23EKqw/exec';
